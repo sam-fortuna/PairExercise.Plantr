dab65a1618efa8ecba4a715bd6f812fe0ac5bc39
Sam Fortuna <saf1119@gmail.com>
2018-08-07 08:43:13 -0500

    updates

--- seed.js
+++ seed.js
@@ -1,4 +1,8 @@
 const { db, Gardener, Plot, Vegetable } = require('./models');
+const PlotVegetable = db.model('vegetable_plot')
+
+// elsewhere
+
 
 // let veggie1 = Vegetable.create({
 //   name: 'Beet',
@@ -30,15 +34,31 @@ db.sync({force: true})
   () => {
     return Vegetable.create(
     { name: 'Beet', color: 'red', plantedon: Date.now() },
-)})
+)} )
 .then(
   (veggies) => {
+    console.log(veggies)
     return Gardener.create({
       name: 'Mr. Gardener',
       age: 56,
       favoriteVegetableId: veggies.id
     })
+  } )
+.then( (gardener) => {
+  console.log(gardener)
+  return Plot.create({
+    size: 376,
+    shaded: true,
+    gardenerId: gardener.id
   })
+.then( (plot) => {
+  return PlotVegetable.create({
+    plotId: plot.id,
+    vegetableId: gardener.favoriteVegetableId
+
+  })
+})
+})
 .catch(
 	err => {
     console.log(err)
